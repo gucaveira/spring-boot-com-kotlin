4dc73813a6b1f1f32b65470fed403159af4fff81
gustavo pereira da rocha <pereiragustavo215@gmail.com>
2022-11-03 18:48:51 -0300

    definindo o controller para Topico

--- forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/controller/HelloController.kt
+++ forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/controller/HelloController.kt
@@ -1,4 +1,4 @@
-package br.com.gustavo.curso.spring.forum
+package br.com.gustavo.curso.spring.forum.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
