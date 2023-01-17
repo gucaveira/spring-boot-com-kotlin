cfdb61c126e536a62a9c4e89de9a451b503a2702
gustavo pereira da rocha <pereiragustavo215@gmail.com>
2023-01-17 11:02:11 -0300

    corrigindo bug da falta de @Entity

--- forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/model/Topico.kt
+++ forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/model/Topico.kt
@@ -1,6 +1,7 @@
 package br.com.gustavo.curso.spring.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
@@ -9,7 +10,7 @@ import javax.persistence.Id
 import javax.persistence.ManyToOne
 import javax.persistence.OneToMany
 
-
+@Entity
 data class Topico(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
--- forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/model/Usuario.kt
+++ forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/model/Usuario.kt
@@ -1,9 +1,11 @@
 package br.com.gustavo.curso.spring.forum.model
 
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
+@Entity
 data class Usuario(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
