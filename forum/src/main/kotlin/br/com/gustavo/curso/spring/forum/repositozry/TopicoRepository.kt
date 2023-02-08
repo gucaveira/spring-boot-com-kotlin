0d78e19f02e0e0d55db4e4951d217e1be8b5a51a
gustavo pereira da rocha <pereiragustavo215@gmail.com>
2023-02-08 17:02:40 -0300

    buscando topico pelo nome do curso

--- forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/repositozry/TopicoRepository.kt
+++ forum/src/main/kotlin/br/com/gustavo/curso/spring/forum/repositozry/TopicoRepository.kt
@@ -4,4 +4,8 @@ import br.com.gustavo.curso.spring.forum.model.Topico
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TopicoRepository : JpaRepository<Topico, Long> {
+
+    // Esse padrão de escrita de funão permite que seja criado
+    // uma queue de SQL, usando as palavras da função
+    fun findByCursoNome(nomeCurso: String): List<Topico>
 }
